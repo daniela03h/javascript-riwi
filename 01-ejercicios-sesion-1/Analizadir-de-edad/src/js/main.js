8b043a5ddc744731b7e7c65a3b8db4b43455c71f
daniela03h <daniela0393h@gmail.com>
2024-04-18 13:40:59 -0500

    se realiza ejercicio de la edad

---
+++ 01-ejercicios-sesion-1/Analizadir-de-edad/src/js/main.js
@@ -0,0 +1,8 @@
+let edad = prompt("Ingresa tu edad: ")
+if (edad < 18){
+    alert("Eres menor de edad")
+} else if ( edad >= 18 && edad <= 24) {
+    alert("Bienvenido a su juventud")
+} else{
+    alert("Estas viejoo")
+}
---
+++ 01-ejercicios-sesion-1/juego-de-adivinanzas/src/js/main.js
@@ -0,0 +1,6 @@
+let numeroSecreto = Math.floor(Math.random() * 100) + 1
+let intentos = 0
+
+while (true) {
+    let suposicion = parseInt(prompt("Ingresa un numero entre 1 y 100: "))
+}
