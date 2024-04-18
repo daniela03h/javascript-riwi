5ea3396f96ae803055105c6b1b6f4a6af05dc815
daniela03h <daniela0393h@gmail.com>
2024-04-18 13:15:47 -0500

    se finalizan ejercicios de operadores

---
+++ 04-ejercicios-sesion-4/ejerciciosOperadores/src/js/main.js
@@ -0,0 +1,181 @@
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
+// 1. ¿Es 35 mayor que 25?
+console.log(35 > 25)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+// 2. ¿Es 45 menor que 30?
+console.log(45 < 30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+// 3. ¿Son 50 y "50" iguales?
+console.log(50 == "50")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+// 4. ¿Es 40 estrictamente igual a 40 convertido a número?
+console.log(40 === 40)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+// 5. ¿Son "apple" y "orange" diferentes?
+console.log("apple" != "orange")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+// 6. ¿Es "apple" estrictamente diferente de "Apple"?
+console.log("apple" !== "Apple")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+// 7. ¿Cuál es el valor de una variable sin definir?
+let variable
+console.log(variable)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+// 8. ¿Cuál es el tipo de dato de una variable sin definir?
+let variable2
+console.log(typeof (variable))
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+// 9. ¿Es null igual a undefined?
+console.log(null == undefined)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 10");
+// Ejercicio 10
+// 10. ¿Son null y undefined estrictamente iguales?
+console.log(null === undefined)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 11");
+// Ejercicio 11
+// 11. ¿Es "2.75" mayor que 2.5?
+console.log("2.75" > 2.5)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 12");
+// Ejercicio 12
+// 12. ¿Es "2.75" mayor que 2.75?
+console.log("2.75" > 2.75)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 13");
+// Ejercicio 13
+// 13. ¿Es "2.75" mayor o igual a 2.75?
+console.log("2.75" >= 2.75)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 14");
+// Ejercicio 14
+// 14. ¿Es "2.75" menor o igual a 2.75?
+console.log("2.75" <= 2.75)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 15");
+// Ejercicio 15
+// 15. ¿Es "2.75" un número mayor a 2.8?
+console.log("2.75" > 2.8)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 16");
+// Ejercicio 16
+// 16. ¿Es "2.75" un número menor a 2.7?
+console.log("2.75" > 2.7)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 17");
+// Ejercicio 17
+// 17. ¿Es el nombre "John" diferente de "john"?
+console.log("John" != "john")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 18");
+// Ejercicio 18
+// 18. ¿Es el nombre "John" diferente de "Jon"?
+console.log("John" != "Jon")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 19");
+// Ejercicio 19
+// 19. ¿Es la edad 30 estrictamente diferente de "30"?
+console.log(30 !== 30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 20");
+// Ejercicio 20
+// 20. ¿Cuál es el resultado de desplazar a la derecha 25 dos veces en bits?
+console.log(25 >> 2)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 21");
+// Ejercicio 21
+// 21. ¿Cuál es el resultado de desplazar a la izquierda 8 tres veces en bits?
+console.log(8 << 3)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 22");
+// Ejercicio 22
+// 22. ¿Son 15 y 15 iguales?
+console.log(15 == 15)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 23");
+// Ejercicio 23
+// 23. ¿Son 20 y 30 diferentes?
+console.log(20 != 30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 24");
+// Ejercicio 24
+// 24. ¿Es "15" mayor que 30?
+console.log("15" > 30)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 25");
+// Ejercicio 25
+// 25. ¿Es 20 menor que 40?
+console.log(20 < 40)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 26");
+// Ejercicio 26
+// 26. ¿Es 100 mayor o igual a "99"?
+console.log(100 >= "99")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 27");
+// Ejercicio 27
+// 27. ¿Es 500 estrictamente igual a "500"?
+console.log(500 === "500")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 28");
+// Ejercicio 28
+// 28. ¿Es 750 estrictamente igual a 750?
+console.log(750 === 750)
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 29");
+// Ejercicio 29
+// 29. ¿Es 5 estrictamente diferente de "5"?
+console.log(5 !== "5")
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 30");
+// Ejercicio 30
+// 30. ¿Es 3 mayor que 5 y 4 menor que 6? (Operadores lógicos)
+console.log(3 > 5 && 4 < 6)
+console.groupEnd();
