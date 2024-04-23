da89893dae2f8e0cba260fa799e398ee6aee59ed
Daniela Londono <daniela0393h@gmail.com>
2024-04-23 17:50:28 -0500

    ejercicios frutas

--- 01-sesion-1/juego-de-adivinanzas/src/js/main.js
+++ 01-sesion-1/juego-de-adivinanzas/src/js/main.js
@@ -3,4 +3,14 @@ let intentos = 0
 
 while (true) {
     let suposicion = parseInt(prompt("Ingresa un numero entre 1 y 100: "))
+    intentos++
+
+    if (suposicion < numeroSecreto) {
+        alert("El numero secreto es mayor")
+    } else if (suposicion > numeroSecreto) {
+        alert("El numero secreto es menor")
+    } else {
+        alert("adivinaste el numero secreto" + numeroSecreto)
+        break
+    }
 }
