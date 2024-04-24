6769bb3db895ef86e8e65b8e9a0eb5ea43d3a30f
Daniela Londono <daniela0393h@gmail.com>
2024-04-24 16:05:12 -0500

    se agrega ejercicios para realiza sesion 8

---
+++ 08-sesion-8/ejerciciosArray/src/js/main.js
@@ -0,0 +1,142 @@
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
+// 1. Crea un array llamado frutas que contenga al menos 5 nombres de frutas diferentes.
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+// 2. Utiliza un bucle for para recorrer el array frutas e imprimir cada elemento en la consola.
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+// 3. Utiliza un foreach para recorrer el array frutas e imprimir cada fruta en mayúscula en la consola.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+// 4. Crea un array llamado números que contenga los números del 1 al 10.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+// 5. Utiliza un bucle while para recorrer el array numeros e imprimir cada elemento en la consola.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+// 6. Agrega una nueva fruta al final del array frutas.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+// 7. Elimina el segundo elemento del array números.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+// 8. Invierte el orden de los elementos en el array frutas.
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+// 9. Ordena el array numeros en orden descendente.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 10");
+// Ejercicio 10
+// 10. Crea una función llamada buscarFruta que tome como argumento una fruta y devuelva true si esa fruta está en el array frutas, y false en caso contrario.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 11");
+// Ejercicio 11
+// 11. Imprime en la consola el primer elemento del array frutas.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 12");
+// Ejercicio 12
+// 12. Imprime en la consola el último elemento del array numeros.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 13");
+// Ejercicio 13
+// 13. Utiliza el método forEach para imprimir cada fruta pero convertida a array.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 14");
+// Ejercicio 14
+// 14. Utiliza un bucle for of para imprimir cada número del array numeros en la consola.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 15");
+// Ejercicio 15
+// 15. Crea un nuevo array llamado duplicados que contenga el doble de cada número en el array numeros utilizando el método map.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 16");
+// Ejercicio 16
+// 16. Crea dos arrays llamados array1 y array2, cada uno con al menos 3 números.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 17");
+// Ejercicio 17
+// 17. Calcula la suma de los elementos de array1 y array2 por separado y muestra los resultados en la consola.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 18");
+// Ejercicio 18
+// 18. Crea una función llamada buscarNumero que tome como argumentos un número y un array de números, y devuelva true si el número está en el array, y false en caso contrario.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 19");
+// Ejercicio 19
+// 19. Crea un array llamado edades que contenga al menos 5 números (edades).
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 20");
+// Ejercicio 20
+// 20. Filtra el array edades para obtener un nuevo array llamado mayoresDeEdad que contenga solo las edades mayores o iguales a 18.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 21");
+// Ejercicio 21
+// 21. Crea un array llamado precios que contenga al menos 3 números (precios de productos).
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 22");
+// Ejercicio 22
+// 22. Utiliza el método map para crear un nuevo array llamado preciosConIVA que contenga los precios con un 21% de IVA aplicado a cada uno.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 23");
+// Ejercicio 23
+// 23. Crea un array llamado duplicados que contenga algunos números duplicados.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 24");
+// Ejercicio 24
+// 24. Elimina los elementos duplicados del array duplicados y muestra el nuevo array sin duplicados en la consola.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 25");
+// Ejercicio 25
+// 25. Crea dos arrays llamados array1 y array2, cada uno con al menos 3 nombres.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 26");
+// Ejercicio 26
+// 26. Concatena los dos arrays en uno nuevo llamado concatenado y muestra el resultado en la consola.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 27");
+// Ejercicio 27
+// 27. Crea un array llamado colores que contenga al menos 5 colores diferentes.
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 28");
+// Ejercicio 28
+// 28. Cuenta cuántos elementos tiene el array colores y muestra el resultado en la consola.
+console.groupEnd();
